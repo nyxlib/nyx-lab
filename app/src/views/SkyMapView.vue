a6a0c49b0cec3bd226d280afe76dc891eb0cfb8f
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2024-04-15 22:50:45 +0200

    update

--- app/src/views/SkyMapView.vue
+++ app/src/views/SkyMapView.vue
@@ -18,8 +18,14 @@ const configStore = useConfigStore();
 
 <template>
 
+    <!-- *********************************************************************************************************** -->
+
     <aladin-map class="h-100 w-100" :fov="2.8" :projection="'SIN'" :show-frame="true" :show-coo-grid="true" :show-coo-location="true" :show-projection-control="true" :show-fullscreen-control="true" v-if="configStore.globals.skymap === 'aladin'" />
 
+    <!-- *********************************************************************************************************** -->
+
     <sky-map v-if="configStore.globals.skymap === 'stellarium'" />
+
+    <!-- *********************************************************************************************************** -->
 
 </template>
