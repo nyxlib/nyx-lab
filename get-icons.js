9f256d16efebd523963bd1f46fda74d16d2de585
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-09-22 15:19:30 +0200

    update

---
+++ get-icons.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+import fs from 'fs';
+import url from 'url';
+import path from 'path';
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const iconsDir = path.join(__dirname, 'node_modules', 'bootstrap-icons', 'icons');
+
+const outFile = path.join(__dirname, 'src', 'assets', 'icons.json');
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+fs.readdir(iconsDir, (err, files) => {
+
+    if(err)
+    {
+        console.error('Error', err);
+
+        return;
+    }
+
+    const icons = files.filter(file => file.endsWith('.svg')).map((file) => path.basename(file, '.svg')).sort();
+
+    fs.writeFile(outFile, JSON.stringify(icons, null, 2), (err) => {
+
+        if(err)
+        {
+            console.error('Error', err);
+
+            return;
+        }
+    });
+});
+
+/*--------------------------------------------------------------------------------------------------------------------*/
