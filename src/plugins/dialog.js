5ed6905de707119210326fb6c9fe48a1ea231035
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2025-04-15 23:52:07 +0200

    update

--- src/plugins/dialog.js
+++ src/plugins/dialog.js
@@ -37,7 +37,7 @@ const _LOCKER_HTML = `
             position: fixed;
 
             top: 40px;
-            left: 00px;
+            bottom: 00px;
 
             left: 4.5rem;
             right: 0.0rem;
