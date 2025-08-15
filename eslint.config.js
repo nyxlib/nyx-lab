6340be68faea4777963b4c154e1639aad625e13d
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2025-08-15 14:19:50 +0200

    update

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default [
             'vue/no-mutating-props': 'off',
             'vue/max-attributes-per-line': 'off',
             'vue/first-attribute-linebreak': 'off',
+            'vue/multi-word-component-names': 'off',
             'vue/no-v-text-v-html-on-component': 'off',
             'vue/multiline-html-element-content-newline': 'off',
             'vue/singleline-html-element-content-newline': 'off',
