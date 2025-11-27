979e2e6d3c3a1d2c282f642db110debb4585fbc7
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2025-11-27 16:10:01 +0100

    update

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -98,7 +98,7 @@ onMounted(() => {
 
     <template v-if="state.indexMode">
 
-        <button class="btn btn-light" type="button" style="z-index: 999999; position: absolute; top: 51px; right: 1rem;" @click="() => { state.indexMode = false; }">
+        <button class="btn btn-light" type="button" style="z-index: 999999; position: absolute; top: 50px; right: 1rem;" @click="() => { state.indexMode = false; }">
             <i class="bi bi-door-open"></i> Close addon index
         </button>
 
