268c1d63a0016711f6cbfb5286bdb03f3aa9a29d
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2025-12-09 14:15:36 +0100

    update

--- src/components/config/ConsoleModal.vue
+++ src/components/config/ConsoleModal.vue
@@ -94,7 +94,7 @@ onUnmounted(() => {
 
                     <div class="modal-body px-3 py-2">
 
-                        <pre class="font-monospace"><code v-for="line in lines" :key="line">{{ `${line.trim()}\n` }}</code></pre>
+                        <pre class="font-monospace overflow-y-scroll" style="max-height: 80vh;"><code v-for="line in lines" :key="line">{{ `${line.trim()}\n` }}</code></pre>
 
                     </div>
 
