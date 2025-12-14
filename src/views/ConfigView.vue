a07c84f0d5d967539be6f0f308c7e695a743ceb9
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2025-12-14 13:35:19 +0100

    update

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -231,11 +231,11 @@ onMounted(() => {
                     <nav-tabs>
 
                         <tab-pane title="Addons">
-                            <addon-table class="shadow-sm" :addons="configStore.globals.addons" :console="configStore.console" @search="() => {state.indexMode = true; }" />
+                            <addon-table class="shadow-sm" :addons="configStore.globals.addons" :console="configStore.console" @search="() => { state.indexMode = true; }" />
                         </tab-pane>
 
                         <tab-pane title="Web pages">
-                            <web-page-table class="shadow-sm" :web-pages="configStore.globals.webPages" @search="() => {state.indexMode = true; }" />
+                            <web-page-table class="shadow-sm" :web-pages="configStore.globals.webPages" @search="() => { state.indexMode = true; }" />
                         </tab-pane>
 
                         <tab-pane title="Cache" @shown="() => state.shownTabs.add('nyx-cache')" v-if="HAS_TAURI">
