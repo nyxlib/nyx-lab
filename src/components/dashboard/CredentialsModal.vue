3dd3a756977635bf870d9186ac6b33fe4244e440
Jérôme ODIER <246020+jodier@users.noreply.github.com>
2026-01-21 12:23:15 +0100

    update

--- src/components/dashboard/CredentialsModal.vue
+++ src/components/dashboard/CredentialsModal.vue
@@ -122,11 +122,11 @@ onUnmounted(() => {
 
                     <!-- ******************************************************************************************* -->
 
-                    <form class="modal-body px-3 py-2" id="A8FAABB1" @submit.prevent="submit">
+                    <form class="modal-body px-3 py-3" id="A8FAABB1" @submit.prevent="submit">
 
                         <div class="row">
                             <div class="col-3 text-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="125" height="125" fill="currentColor" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="135" height="135" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m0 5.996V14H3s-1 0-1-1 1-4 6-4q.845.002 1.544.107a4.5 4.5 0 0 0-.803.918A11 11 0 0 0 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664zM9 13a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1" />
                                 </svg>
                             </div>
