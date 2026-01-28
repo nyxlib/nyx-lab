b096a8e29ac021d7fb7c196d60ed9db810a30c72
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2026-01-28 16:46:03 +0100

    update

--- src/components/dashboard/ControlCanvas.vue
+++ src/components/dashboard/ControlCanvas.vue
@@ -89,7 +89,7 @@ onUnmounted(() => {
                         <div class="list-group-item" v-if="['variable', 'stream'].includes(widget.mode)">
                             <div class="ms-1 me-auto">
                                 <div class="fw-bold">
-                                    {{ widget.title }}
+                                    {{ widget.title || '/' }}
                                 </div>
                                 <div class="form-check form-switch" v-for="index2 in widget.variables1.length" :key="index2">
                                     <input class="form-check-input" type="checkbox" :id="`FE664D_${index1}${index2}`" v-model="widget.enabled[widget.variables1[index2 - 1]]">
@@ -105,7 +105,7 @@ onUnmounted(() => {
                         <div class="list-group-item" v-if="['scatter'].includes(widget.mode)">
                             <div class="ms-1 me-auto">
                                 <div class="fw-bold">
-                                    {{ widget.title }}
+                                    {{ widget.title || '/' }}
                                 </div>
                                 <div class="form-check form-switch" v-for="index2 in Math.min(widget.variables1.length, widget.variables2.length)" :key="index2">
                                     <input class="form-check-input" type="checkbox" :id="`FE664D_${index1}${index2}`" v-model="widget.enabled[`${widget.variables1[index2 - 1]} / ${widget.variables2[index2 - 1]}`]">
