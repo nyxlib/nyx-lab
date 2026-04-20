a04793f3ed326218b74ed3524d265db2f59d92eb
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2026-04-20 12:03:22 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -244,7 +244,7 @@ const openHelp = () => {
 
                 <!-- *********************************************************************************************** -->
 
-                <div class="d-none d-md-block position-absolute text-center opacity-50" style="font-size: 0.9rem; bottom: 8px;">
+                <div class="d-none d-md-block position-absolute text-center opacity-50" style="font-size: 0.9rem; bottom: 4px;">
 
                     <span>
                         © 2024-{{ new Date().getFullYear() }} <a href="https://nyxlib.org/" target="_blank">Nyx Lab</a> — <a href="https://odier.xyz/" target="_blank">Jérôme ODIER</a>, <a href="https://lpsc.in2p3.fr/" target="_blank">LPSC</a> / <a href="https://cnrs.fr/" target="_blank">CNRS</a>.
