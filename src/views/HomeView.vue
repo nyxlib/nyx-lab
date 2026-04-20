5c6c95638a8bf3e7950ac3e23ea36e3b5e1e0d94
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2026-04-20 10:25:53 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -186,9 +186,9 @@ const openHelp = () => {
     <!-- DASHBOARD                                                                                                   -->
     <!-- *********************************************************************************************************** -->
 
-    <div class="overflow-y-hidden overflow-y-auto h-100 w-100 p-3">
+    <div class="d-flex flex-column h-100 w-100 p-3">
 
-        <nyx-dashboard class="position-relative" :show-devices="configStore.globals.showNyxInterfaces" @connect="connect" @disconnect="disconnect">
+        <nyx-dashboard :show-devices="configStore.globals.showNyxInterfaces" @connect="connect" @disconnect="disconnect">
 
             <!-- *************************************************************************************************** -->
 
@@ -244,7 +244,7 @@ const openHelp = () => {
 
                 <!-- *********************************************************************************************** -->
 
-                <div class="d-none d-md-block position-absolute text-center opacity-50" style="font-size: 0.9rem; bottom: -10px;">
+                <div class="d-none d-md-block position-absolute text-center opacity-50" style="font-size: 0.9rem; bottom: 8px;">
 
                     <span>
                         © 2024-{{ new Date().getFullYear() }} <a href="https://nyxlib.org/" target="_blank">Nyx Lab</a> — <a href="https://odier.xyz/" target="_blank">Jérôme ODIER</a>, <a href="https://lpsc.in2p3.fr/" target="_blank">LPSC</a> / <a href="https://cnrs.fr/" target="_blank">CNRS</a>.
